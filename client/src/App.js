5bf87b1726da10cc6a227e029f106d8e097e5955
Marta <54746702+MartaYardin@users.noreply.github.com>
2022-01-10 10:40:41 -0300

    arreglo

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { Header } from './components/Header/Header';
 import { Hero } from './components/Hero/Hero';
-import { InfoSlider } from './components/InfoSlider/InfoSlider'
+import { InfoSlider } from './components/InfoSlider/InfoSlider';
+import { Footer } from './components/Footer/Footer';
 function App() {
 	return (
 		<div className='App'>
 			<Header />
 			<Hero />
 			<InfoSlider />	
+			<Footer />
 		</div>
 	);
 }
