abd2e88b657367ce9eeaa60e52043369c190fa8a
Obed Paz <lesterpaz99@gmail.com>
2022-01-09 21:20:05 -0600

    agrega el Footer

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Auth0Provider } from "@auth0/auth0-react";
-const{REACT_APP_AUTH0_CLIENT_ID, REACT_APP_AUTH0_DOMAIN} = process.env
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+const { REACT_APP_AUTH0_CLIENT_ID, REACT_APP_AUTH0_DOMAIN } = process.env;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain={REACT_APP_AUTH0_DOMAIN}
-      clientId={REACT_APP_AUTH0_CLIENT_ID}
-      redirectUri={window.location.origin}
-    >
-      <App />
-    </Auth0Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<Auth0Provider
+			domain={REACT_APP_AUTH0_DOMAIN}
+			clientId={REACT_APP_AUTH0_CLIENT_ID}
+			redirectUri={window.location.origin}
+		>
+			<App />
+		</Auth0Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
