9efaa044133659137e4a5ef9664c04ecc9ccada5
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-01-09 21:35:12 -0300

    limpieza de codigo

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import { Login } from './components/Login-Logout/Login';
-import { Logout } from './components/Login-Logout/Logout';
 import { Header } from './components/Header/Header';
 import { Hero } from './components/Hero/Hero';
 import { InfoSlider } from './components/InfoSlider/InfoSlider'
@@ -9,8 +7,7 @@ function App() {
 		<div className='App'>
 			<Header />
 			<Hero />
-			<InfoSlider />
-			<Login /> <Logout />
+			<InfoSlider />	
 		</div>
 	);
 }
