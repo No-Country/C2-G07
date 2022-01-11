210f4f2cb627a8a14a87e364d93edcf9d5b635b5
MartaYardin <martayardin@gmail.com>
2022-01-11 15:29:07 -0300

    corrección texto

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,12 +1,7 @@
 import styles from './Hero.module.css' ;
 
 import humo from '../../assets/img/A-Cluster-Of-Curling-Pink-.png';
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-=======
 import { ButtonPrimaryLink } from '../ButtonPrimaryLink/ButoonPrimaryLink';
->>>>>>> develop
 const Hero = () =>{
     return (
         <section>
@@ -14,11 +9,8 @@ const Hero = () =>{
                 <article>
                     <div className={styles.titles}>
                         <h2 className={styles.mainTitle}> Una Web creada para artistas</h2>
-<<<<<<< HEAD
-                        <h3> Regístrate y podrás subir fotos y tener tu portfolio</h3>
-=======
-                        <h3>Regístrate y podras subir tus fotos y tener tu portfolio</h3>
->>>>>>> develop
+
+                        <h3>Regístrate y podrás subir fotos y tener tu portfolio</h3>
                     </div>
                     <p>Así de simple</p>
                     <ButtonPrimaryLink name="Registrarse"/>
