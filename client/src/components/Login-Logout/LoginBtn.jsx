f50d26c26f49043fa459bb2e4449bce1b442815f
Obed Paz <lesterpaz99@gmail.com>
2022-01-10 23:22:32 -0600

    crea los componentes botones de Login y Logout

---
+++ client/src/components/Login-Logout/LoginBtn.jsx
@@ -0,0 +1,16 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import styles from './LoginLogout.module.css';
+
+const LoginBtn = () => {
+	const { loginWithRedirect } = useAuth0();
+
+	const handleLogin = () => loginWithRedirect();
+
+	return (
+		<button className={styles.loginBtn} onClick={handleLogin}>
+			Login / Sign up
+		</button>
+	);
+};
+
+export { LoginBtn };
