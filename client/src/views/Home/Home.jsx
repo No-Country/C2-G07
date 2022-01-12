7096bdc7eab8c73f57b5c2ace6ea2d94098f4003
Ivan Sotelo <soteloivanmanuel26@gmail.com>
2022-01-12 15:59:36 -0300

    se resuelve conflicto

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import {getObrasArtes} from '../../redux/actions/index'
-import { useEffect , useState} from 'react'
-import{ useSelector, useDispatch } from 'react-redux';
-import { Cards } from '../../components/card/Card';
-import styles from './Home.module.css'
-=======
 import {getObrasArtes} from '../../redux/actions/index';
 import { useEffect , useState} from 'react';
 import{ useSelector, useDispatch } from 'react-redux';
 import { Cards } from '../../components/card/Card';
 import styles from './Home.module.css';
->>>>>>> views
 
 export default function Home(){
 	const obrasArtes = useSelector((state) => state.obraArtes)
