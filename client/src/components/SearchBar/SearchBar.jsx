b23fb253d2f093dc13d127f52b22db303c0a7ddb
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-01-13 09:32:42 -0300

    ordenamiento por nombre agregado

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import { getObrasArtesName, getObrasArtes } from "../../redux/actions/index";
 import { useDispatch } from "react-redux";
+
+
 export const SearchBar = () => {
   const [obra, setObra] = useState();
   const dispatch = useDispatch();
-  
+ const [order, setOrder] = useState("asc");
 
   useEffect(() => {
     if (obra) {
@@ -15,6 +17,11 @@ export const SearchBar = () => {
       dispatch(getObrasArtes("asc"));
     }
   }, [dispatch, obra]);
+
+  function handleChangeNombreObra(e){
+    setOrder(e.target.value);
+    dispatch(getObrasArtes(e.target.value));
+  };
   return (
     <span>
       <input
@@ -23,6 +30,10 @@ export const SearchBar = () => {
         value={obra}
         onChange={(e) => setObra(e.target.value)}
       />
+      <select value={order} onChange={handleChangeNombreObra}>
+            <option value={"ASC"}>ASC</option>
+            <option value={"DESC"}>DESC</option>
+        </select>
     </span>
   );
 };
