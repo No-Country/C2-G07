bdc175b74d286188871c8b47792eb3b65d0041fa
Obed Paz <lesterpaz99@gmail.com>
2022-01-15 00:56:25 -0600

    refactoriza codigo en la logica de redux y soluciona algunos typos

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,72 +1,69 @@
 import {
-  GET_OBRAS_ARTES,
-  GET_OBRAS_ARTES_NAME,
-  GET_PERFIL_USUARIO,
-  GET_OBRAS_USUARIO,
-  GET_INFORMACION_OBRA_ARTE,
-  GET_CATEGORIA,
-  GET_USUARIOS,
+	GET_OBRAS_ARTES,
+	GET_OBRAS_ARTES_NAME,
+	GET_PERFIL_USUARIO,
+	GET_OBRAS_USUARIO,
+	GET_INFORMACION_OBRA_ARTE,
+	GET_CATEGORIA,
+	GET_USUARIOS,
+} from '../actions/index';
 
-
-} from "../actions/index";
-
-const initalState = {
-  obraArtes: [],
-   usuario: {},
-   obrasUsuario:[],
-   obraArte:{},
-   categorias:[],
-   usuarios:[],
-
+const initialState = {
+	obraArtes: [],
+	usuario: {},
+	obrasUsuario: [],
+	obraArte: {},
+	categorias: [],
+	usuarios: [],
 };
 
-export default function rootReducer(state = initalState, action) {
-  if (action.type === GET_OBRAS_ARTES) {
-    return {
-      ...state,
-      obraArtes: action.payload,
-    };
-  }
+export default function rootReducer(state = initialState, action = {}) {
+	if (action.type === GET_OBRAS_ARTES) {
+		return {
+			...state,
+			obraArtes: action.payload,
+		};
+	}
 
-  if (action.type === GET_OBRAS_ARTES_NAME) {
-    return {
-      ...state,
-      obraArtes: action.payload,
-    };
-  }
+	if (action.type === GET_OBRAS_ARTES_NAME) {
+		return {
+			...state,
+			obraArtes: action.payload,
+		};
+	}
 
-  if (action.type === GET_PERFIL_USUARIO) {
-    return {
-      ...state,
-      usuario: action.payload,
-    };
-  }
+	if (action.type === GET_PERFIL_USUARIO) {
+		return {
+			...state,
+			usuario: action.payload,
+		};
+	}
 
-  if (action.type === GET_OBRAS_USUARIO) {
-    return {
-      ...state,
-      obrasUsuario: action.payload,
-    };
-  }
+	if (action.type === GET_OBRAS_USUARIO) {
+		return {
+			...state,
+			obrasUsuario: action.payload,
+		};
+	}
 
-  if (action.type === GET_USUARIOS) {
-    return{
-      ...state,
-      usuarios: action.payload,
-    };
-  }
-  if (action.type === GET_INFORMACION_OBRA_ARTE) {
-    return {
-      ...state,
-      obraArte: action.payload,
-    };
-  }
-  if (action.type === GET_CATEGORIA) {
-    return {
-      ...state,
-      categorias: action.payload,
-    };
-  }
+	if (action.type === GET_USUARIOS) {
+		return {
+			...state,
+			usuarios: action.payload,
+		};
+	}
+	if (action.type === GET_INFORMACION_OBRA_ARTE) {
+		return {
+			...state,
+			obraArte: action.payload,
+		};
+	}
+	if (action.type === GET_CATEGORIA) {
+		return {
+			...state,
+			categorias: action.payload,
+		};
+	}
 
-  return state;
+	return state;
 }
