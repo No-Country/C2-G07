4c4ae2d8137fb7b91b5a9ee89badcbca1e85842c
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-01-26 16:28:06 -0300

    correccion

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,37 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import Home from './views/Home/Home';
-import Usuarios from './views/Usuarios/Usuarios';
-import { Header } from './components/Header/Header';
-import { AboutUs } from './views/AboutUs/AboutUs';
-import { Landing } from './views/Landing/Landing';
-import { Footer } from './components/Footer/Footer';
-import { Routes, Route } from 'react-router-dom';
-import { PerfilUsuario } from './views/PerfilUsuario/PerfilUsuario';
-import { InformacionObraArte } from './views/informacionObraArte/InformacionObraArte';
-import { AgregarObraArte } from './views/AgregarObraArte/AgregarObraArte';
-import { EditarPerfil } from './views/EditarPerfil/EditarPerfil';
-import { Loader } from './components/Loader/Loader';
+import "./App.css";
+import { Header } from "./components/Header/Header";
+import { AboutUs } from "./views/AboutUs/AboutUs";
+import Home from "./views/Home/Home";
+import { Landing } from "./views/Landing/Landing";
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { Footer } from "./components/Footer/Footer";
+import { PerfilUsuario } from "./views/PerfilUsuario/PerfilUsuario";
+import Usuarios from "./views/Usuarios/Usuarios";
+import { InformacionObraArte } from "./views/informacionObraArte/InformacionObraArte";
+import { AgregarObraArte } from "./views/AgregarObraArte/AgregarObraArte";
+import { EditarPerfil } from "./views/EditarPerfil/EditarPerfil";
 
 
 function App() {
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		setTimeout(() => {
-			setLoading(false);
-		}, 3500);
-	});
-
-	if (loading) return <Loader />;
-
 	return (
 		<div className='App'>
 			<Header />
 			<Routes>
 				<Route path='/' element={<Landing />} />
+				
 				<Route path='/galeria' element={<Home />} />
-				<Route path='usuarios' element={<Usuarios />} />
-				<Route path='/usuario/:id' element={<PerfilUsuario />} />
-				<Route path='/obraArte/:id' element={<InformacionObraArte />} />
+        		<Route path='usuarios' element={<Usuarios />} />
+				<Route path='/usuario/:id' element={<PerfilUsuario/>}/>
+				<Route path='/obraArte/:id' element={<InformacionObraArte/>}/>
 				<Route path='/aboutUs' element={<AboutUs />} />
-				<Route path='/nuevaObraArte' element={<AgregarObraArte />} />
-				<Route path='/editarPerfil/:id' element={<EditarPerfil />} />
+				<Route path='/nuevaObraArte' element={<AgregarObraArte/>}/>
+				<Route path='/editarPerfil/:id' element={<EditarPerfil/>}/>
 			</Routes>
 			<Footer />
-		</div>
-	);
+	    </div>
+  	);
+
 }
 
 export default App;
