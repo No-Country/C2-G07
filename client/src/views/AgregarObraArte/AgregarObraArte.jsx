935f6b5403901d11fca3d06d8810899364318964
Ivan Sotelo <soteloivanmanuel26@gmail.com>
2022-01-26 02:26:53 -0300

    se hace responsive vista de agregar obra de arte

--- client/src/views/AgregarObraArte/AgregarObraArte.jsx
+++ client/src/views/AgregarObraArte/AgregarObraArte.jsx
@@ -77,69 +77,91 @@ export const AgregarObraArte = () => {
     <div className={styles.container}>
       <h1>Publicar una nueva obra de arte</h1>
       <form onSubmit={(e) => handleSubmit(e)} className={styles.containerForm}>
-        <input
-          type="text"
-          name="oa_name"
-          placeholder="Nombre de la obra)"
-          value={input.oa_name}
-          onChange={handleInputChange}
-          required={true}
-          className={styles.input}
-        />
-        <input
-          type="text"
-          name="oa_resenia"
-          placeholder="Reseña (max 300 caracteres)"
-          value={input.oa_resenia}
-          onChange={handleInputChange}
-          required={true}
-          className={styles.input}
-        />
-        <input
-          type="text"
-          name="oa_descripcion"
-          placeholder="Descripcion..."
-          value={input.oa_descripcion}
-          onChange={handleInputChange}
-          required={true}
-          className={styles.input}
-        />
-        <div className={styles.containerFecha}>
-          <p >Fecha creación: </p>
-          <input
-            type="date"
-            name="oa_fechaCreacion"
-            value={input.oa_fechaCreacion}
-            onChange={handleInputChange}
-            required={true}
-            className={styles.inputDate}
-          />
+        <div className={styles.containerBlocks}>
+          <div className={styles.containerInputs}>
+            <label htmlFor="oa_name">Nombre de la obra</label>
+            <input
+              type="text"
+              name="oa_name"
+              placeholder="Ingrese nombre"
+              value={input.oa_name}
+              onChange={handleInputChange}
+              required={true}
+              className={styles.input}
+            />
+          </div>
+          <div className={styles.containerInputs}>
+            <label htmlFor="oa_resenia">Reseña</label>
+            <input
+              type="text"
+              name="oa_resenia"
+              placeholder="Máx. 300 caracteres"
+              value={input.oa_resenia}
+              onChange={handleInputChange}
+              required={true}
+              className={styles.input}
+            />
+          </div>
         </div>
-        <select
-          onChange={handleInputChange}
-          name="cat_id"
-          className={styles.input}
-        >
-          {categorias.map((cat) => (
-            <option value={cat.cat_id} selected="selected">
-              {cat.cat_descripcion}
-            </option>
-          ))}
-        </select>
-        <div>
-          <input
-            type="file"
-            name="file"
-            placeholder="Carga una imagen"
-            value={input.file}
-            onChange={uploadImage}
-          />
-
-          <form action="" method="post">
-            <input type="text" value={url} hidden={true} />
-          </form>
-          <div>{loading ? <Loader /> : <img src={image} alt="" height={250} />}</div>
+        <div className={styles.containerBlocks}>
+          <div className={styles.containerInputs}>
+            <label htmlFor="oa_descripcion">Descripción</label>
+            <input
+              type="text"
+              name="oa_descripcion"
+              placeholder="Ingrese descripción"
+              value={input.oa_descripcion}
+              onChange={handleInputChange}
+              required={true}
+              className={styles.input}
+            />
+          </div>
+          <div className={styles.containerFecha}>
+            <label htmlFor="oa_fechaCreacion">Fecha de creación</label>
+            <input
+              type="date"
+              name="oa_fechaCreacion"
+              value={input.oa_fechaCreacion}
+              onChange={handleInputChange}
+              required={true}
+              className={styles.inputDate}
+            />
+          </div>
         </div>
+        <div className={styles.containerBlocks}>
+            <div className={styles.containerInputs}>
+              <label htmlFor="cat_id">Elija una categoría</label>
+              <select
+                onChange={handleInputChange}
+                name="cat_id"
+                className={styles.input}
+              >
+                {categorias.map((cat) => (
+                  <option value={cat.cat_id} selected="selected">
+                    {cat.cat_descripcion}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className={styles.containerInputs}>
+              <div className={styles.containerInputFile}>
+                <div className={styles.inputFile}>
+                  <input
+                    type="file"
+                    name="file"
+                    placeholder="Carga una imagen"
+                    value={input.file}
+                    required={true}
+                    onChange={uploadImage}
+                  />
+                </div>  
+              </div>
+                {/* <form action="" method="post"> */}
+                <input type="text" value={url} hidden={true} />
+                {/* </form> */}
+            </div>
+        </div>
+          <div className={styles.containerImage}>{loading ? <Loader /> : <img src={image} alt="" height={250} />}</div>
         <input type="submit" className={styles.buttonSubmit} />
       </form>
     </div>
