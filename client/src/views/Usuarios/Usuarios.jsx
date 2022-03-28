ac773e06ece951275ba9ce43803fff4e7cd49f66
facundomaksud@gmail.com <facundomaksud@gmail.com>
2022-03-28 15:32:58 -0300

    estilos vista usuarios agregados

--- client/src/views/Usuarios/Usuarios.jsx
+++ client/src/views/Usuarios/Usuarios.jsx
@@ -1,31 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { getUsuarios } from '../../redux/actions/index';
-import { useSelector, useDispatch } from 'react-redux';
-import { BoxUsuario } from '../../components/BoxUsuario/BoxUsuario';
-import styles from './Usuarios.module.css';
-// import { SearchBar } from '../../components/SearchBar/SearchBar';
+import React, { useEffect, useState } from "react";
+import { getUsuarios } from "../../redux/actions/index";
+import { useSelector, useDispatch } from "react-redux";
+import { BoxUsuario } from "../../components/BoxUsuario/BoxUsuario";
+import styles from "./Usuarios.module.css";
 
 export default function Usuarios() {
-	const [order, setOrder] = useState('asc');
-	const usuarios = useSelector((state) => state.usuarios);
-	const dispatch = useDispatch();
+  const [order, setOrder] = useState("asc");
+  const usuarios = useSelector((state) => state.usuarios);
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(getUsuarios(order));
-	}, [dispatch, order]);
+  useEffect(() => {
+    dispatch(getUsuarios(order));
+  }, [dispatch, order]);
 
-	return (
-		<article className={styles.container}>
-			{usuarios.length > 0
-				? usuarios.map((user, index) => (
-						<BoxUsuario
-							key={index}
-							id={user.usuario_id}
-							name={user.usuario_name}
-							usuarioImagen={user.usuario_imagen}
-						/>
-				  ))
-				: null}
-		</article>
-	);
+  return (
+    <div>
+      <h1 className={styles.title}>ARTISTAS</h1>
+      <article className={styles.container}>
+        {usuarios.length > 0
+          ? usuarios.map((user, index) => (
+              <BoxUsuario
+                key={index}
+                id={user.usuario_id}
+                name={user.usuario_name}
+                usuarioImagen={user.usuario_imagen}
+              />
+            ))
+          : null}
+      </article>
+    </div>
+  );
 }
